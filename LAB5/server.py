957f294340d37a6a2f2671f2d8f3f792aeecd9bb
P.V.S.PRANAV <141631188+pranav-iiitdm@users.noreply.github.com>
2023-09-14 15:56:25 +0530

    Update server.py

--- LAB5/server.py
+++ LAB5/server.py
@@ -1,3 +1,29 @@
+# File Transfer: SERVER
+# The server performs the following functions:
+# Create a TCP socket.
+# Bind the IP address and PORT to the server socket.
+# Listening for the clients.
+# Accept the connection from the client.
+# Receive the filename from the client and create a text file.
+# Send a response back to the client.
+# Receive the text data from the client.
+# Write (save) the data into the text file.
+# Send a response message back to the client.
+# Close the text file.
+# Close the connection.
+
+# FIle Transfer: CLIENT
+# The client performs the following functions:
+# Create a TCP socket for the client.
+# Connect to the server.
+# Read the data from the text file.
+# Send the filename to the server.
+# Receive the response from the server.
+# Send the text file data to the server.
+# Receive the response from the server.
+# Close the file.
+# Close the connection.
+
 import socket
 import threading
 
