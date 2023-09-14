4a36af7b0d3b2f9a3c5bc7a6d343ecb591e9a6be
P.V.S.PRANAV <141631188+pranav-iiitdm@users.noreply.github.com>
2023-09-14 15:55:41 +0530

    Update server.py

--- LAB3/server.py
+++ LAB3/server.py
@@ -1,3 +1,13 @@
+# Write a Server program with threads, where each thread can handle a single client.
+# Ensure server get connected to multiple clients.
+# All the client messages to be displayed in server.
+# Provide a mechanism to disconnect the client.
+
+# Implement with python socket programming.
+# 1) Demonstrate with local loop IP
+# 2) Connect with multiple client with different IPs
+# 3) Modify the program where server can send messages to specific client.
+
 import socket
 import threading
 import sys
